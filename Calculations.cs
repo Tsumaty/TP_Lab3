148786f3ed5e9256afaba78edddcc8aca1ce6b38
Tsumaty <Tsumaty@TSUMATYLAPTOP>
2024-03-06 11:40:45 +0300

    Добавьте файлы проекта.

---
+++ Calculations.cs
@@ -0,0 +1,67 @@
+﻿// using System.Globalization;
+
+namespace TP_Lab3
+{
+    internal class Calculations : Exception
+    {
+        internal static double Func1(double x)
+        {
+            return Math.Sin(-x * x);
+        }
+
+        internal static double Func2(double x)
+        {
+            if (x == 0.0)
+            {
+                throw new Exception("Error in func2! Division by zero!");
+            }
+            double num = 1.0 / Math.Pow(x, 3.0) - 1.0;
+            if (num < 0)
+            {
+                throw new Exception("Error in func2! Negative number under the root!");
+            }
+            return -Math.Sqrt(num);
+        }
+
+        internal static double Func3(double x)
+        {
+            if (x <= 0.0)
+            {
+                throw new Exception("Error in func3! Negative number or zero in the logarithm!");
+            }
+            else if (x == 1.0)
+            {
+                throw new Exception("Error in func3! Division by zero!");
+            }
+            return Math.Exp(-Math.Sin(5.0 / Math.Log(x)));
+        }
+
+        internal static double Func4(double x)
+        {
+            if (x == 0.0)
+            {
+                throw new Exception("Error in func4! Division by zero!");
+            }
+            double sum = 0.0;
+            for (int j = 1; j <= 1000000; j++)
+                sum += 1.0 / x + Math.Sqrt(j);
+            return sum;
+        }
+
+        internal static double Func(double x)
+        {
+            /*try
+            {*/
+            double f3 = Func3(x);
+            double f4 = Func4(x);
+            double f2 = Func2(x);
+            double f1 = Func1(x);
+            return f1 + f2 + f3 + f4;
+            /*}
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }*/
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿// var 3
+// sin(−x^2) | −\/(1/x^3−1) | exp(−sin(5/ln(x))) | foreach
+
+using System.Globalization;
+
+namespace TP_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // массив параметров функции - целые числа, которые
+            // впоследситвии будут поделены на 1000.0,
+            // чтобы уменьшить погрешность чисел типа double
+            int[] array = new int[1001];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i - 5;
+            }
+
+            List<string> calcResults = new List<string>();
+
+            // текст предыдущего исключения для вывода
+            // только первого уникального исключения
+            string exPrevMessage = "";
+
+            // просим пользователя подождать
+            Console.Write("Please, wait several seconds..");
+            // вывод в консоль и заполнение списка
+            // циклом foreach (по заданию)
+            foreach (double num in array)
+            {
+                // парметр n - это число из массива array, делённое
+                // на 1000.0 для максимальной точности вычислений
+                double n = num / 1000.0;
+                // переводим n в строку для ровного вывода с привязкой к правому краю
+                string x = n.ToString("f3", CultureInfo.InvariantCulture).PadLeft(6);
+                // создаём новую строку вида "x: y(x)"
+                string xy = x + ": ";
+
+                try
+                {
+                    // функция вычсиляется с параметром n
+                    double f = Calculations.Func(n);
+                    // перевод в строку для более красивого формата
+                    string y = f.ToString("0.000000E+00", CultureInfo.InvariantCulture);
+
+                    xy += y;
+
+                    // текст прошлого исключения обнуляется,
+                    // если вычисления прошли успешно
+                    exPrevMessage = "";
+
+                    // добавляем аргумент и значение функции
+                    // в список для занесения его в файл
+                    calcResults.Add(xy);
+
+                    // выводим результаты в консоль
+                    // Console.WriteLine($"{xy}");
+                }
+                catch (Exception ex)
+                {
+                    // вывод исключения только если текст исключения
+                    // отличается от предыдущего
+                    if (ex.Message != exPrevMessage)
+                    {
+                        xy += ex.Message;
+                        calcResults.Add(xy);
+                        // Console.WriteLine($"{x}: {ex.Message}");
+                        exPrevMessage = ex.Message;
+                    }
+                }
+            }
+
+            // запись аргументов и значений функции в файл
+            try
+            {
+                File.WriteAllLines("calcResults.txt", calcResults);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
